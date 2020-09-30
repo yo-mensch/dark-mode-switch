ca97db28a3f4bf7ab266544db66ee1999e0a83c9
Rasa Jurgutytė <53710574+yo-mensch@users.noreply.github.com>
2020-09-30 21:16:33 +0300

    finished styling

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("All good");
+//alert("All good");
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+function switchTheme(event){
+    console.log(event);
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
