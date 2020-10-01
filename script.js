acb019bda084dcf8fea2a1d505406c4fa74c50a5
Rasa Jurgutytė <53710574+yo-mensch@users.noreply.github.com>
2020-10-01 14:05:25 +0300

    improved switching

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ function switchTheme(event){
     // console.log(event.target.checked);
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else{
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
@@ -43,3 +45,13 @@ function lightMode(){
 }
 
 toggleSwitch.addEventListener('change', switchTheme);
+
+//check local storage
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if(currentTheme==='dark'){
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
